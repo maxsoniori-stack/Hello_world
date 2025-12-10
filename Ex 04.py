deae33aff6b73a59a344e737086251078e54c2c2
maxsoniori-stack <maxsoniori@gmail.com>
2025-12-10 01:01:59 +0000

    ex 04.py

---
+++ Ex 04.py
@@ -0,0 +1,12 @@
+a = int(input("Digite o primeiro numero: "))
+b = int(input("Digite o segundo numero: "))
+
+multiplicação = a * b
+soma = a + b
+subtração = a - b
+divisão = a //  b
+print("resultado")
+print(f"{a} x {b} = {multiplicação}")
+print(f"{a} + {b} = {soma}")
+print(f"{a} - {b} = {subtração}")
+print(f"{a} / {b} = {divisão}")
