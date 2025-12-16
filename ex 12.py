941e09cc34d23e3a912beb08beeb971e4135c549
maxsoniori-stack <maxsoniori@gmail.com>
2025-12-16 01:03:29 +0000

    ex12

---
+++ ex 12.py
@@ -0,0 +1,10 @@
+lista_numeros = [15,20,571]
+a = lista_numeros[0]
+b = lista_numeros[1]
+c= lista_numeros[2]
+print("A soma de todos os numeros é: " + str(a+b+c))
+print("Multiplicação desses numeros é:" + str(a*b*c))
+print("A divisão desses numeros é: " + str(a/b/c))
+print(" Esses numeros ao quadrado é " + str(a*a))
+print(" Esses numeros ao quadrado é " + str(b*b))
+print(" Esses numeros ao quadrado é " + str(c*c))
